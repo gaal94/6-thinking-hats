cf2b35b6ff39ed5aff44419a9d03563dd4a8854d
최지원 <weejee0605@gmail.com>
2022-07-28 10:36:56 +0900

    [TEST] jw push test

--- 6hats/src/main/java/com/ssafy/sixhats/gitTest.java
+++ 6hats/src/main/java/com/ssafy/sixhats/gitTest.java
@@ -3,5 +3,6 @@ package com.ssafy.sixhats;
 public class gitTest {
     public static void test (){
         System.out.println("System.out.println은 sout");
+        System.out.println("git push test");
     }
 }
