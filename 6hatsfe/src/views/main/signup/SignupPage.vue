bd3eb6100ea08f7241437b6535a30472dbc8c76f
황석영 <sjm01065@naver.com>
2022-08-03 13:33:40 +0900

    [FEAT] signup frame

--- 6hatsfe/src/views/main/signup/SignupPage.vue
+++ 6hatsfe/src/views/main/signup/SignupPage.vue
@@ -15,6 +15,17 @@
         <input type="radio" v-model="gender" value="남">남
         <input type="radio" v-model="gender" value="여">여
         <br>
+        생년월일
+        <input type="date" v-model="birth"/>
+        <br>
+        직업
+        <select v-model="job">
+  <option value="직장인">직장인</option>
+  <option value="학생" selected>학생</option>
+  <option value="무직">무직</option>
+  <option value="direct">기타</option>
+</select>
+<input type="text" id="selboxDirect" name="selboxDirect"/>
         <button type="submit">회원가입</button>
       </form>
   </div>
@@ -33,7 +44,10 @@ export default {
     onSubmit () {
       console.log(this.email)
       console.log(this.password)
-      console.log(this.gender)
+          console.log(this.gender)
+          console.log(this.birth)
+          console.log(this.job)
+      alert("")
     }
   }
 
