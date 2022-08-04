df0615f3fdd3a6f952850685df7f6d5e404b0bc0
gnusho <osj2387@naver.com>
2022-08-04 16:11:56 +0900

    [FEAT] modify jwtinterceptor to avoid CORS error

--- 6hatsBE/src/main/java/com/ssafy/sixhats/config/Config.java
+++ 6hatsBE/src/main/java/com/ssafy/sixhats/config/Config.java
@@ -3,6 +3,7 @@ package com.ssafy.sixhats.config;
 import com.ssafy.sixhats.interceptor.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
