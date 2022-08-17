25690b5fc4640187ff65991d6146a4dee4ec1ac4
최지원 <weejee0605@gmail.com>
2022-08-17 11:30:52 +0900

    [FIX] QnaPage footer

--- 6hatsfe/src/views/main/qna/QnaPage.vue
+++ 6hatsfe/src/views/main/qna/QnaPage.vue
@@ -1,52 +1,52 @@
 <template>
-  <div id="recgo">
-    <header class="pagename">
-      <h1>QnA</h1>
-    </header>
-    <table class="table">
-      <thead class="Rechead">
-        <tr class="headrow">
-          <th scope="col">글번호</th>
-          <th scope ="col" colspan="4">제목</th>
-          <th scope="col">작성자</th>
-          <th scope="col">작성일</th>
-          <th scope="col">조회수</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(no,idx) in boards" :key ="idx">
-          <td scope="row">{{pageNum * pageSize + idx + 1}}</td>
-          <td colspan="4"><router-link :to ="{
-              path: '/qnacontentspage/' + no.boardId
-            }" style="color: #000000">
-          {{no.title}}</router-link></td>
-          <td>{{no.name}}</td>
-          <td>{{no.boardCreatedAt}}</td>
-          <td>{{no.views}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <nav aria-label="Page navigation example" style="display:flex; justify-content: center; padding:5px;">
-      <ul class="pagination">
-        <li class="page-item">
-          <a v-on:click="substractPageNum" class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">{{ pageNum + 1}}</a></li>
-        <li class="page-item">
-          <a v-on:click="addPageNum" class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
-    </nav>    
-    <div class ="boardbtn">
-      <button v-on:click="routeToWritePage" type="button" class="btn btn-primary" id="boardwritingbtn">글쓰기</button>
+    <div id="recgo">
+        <header class="pagename">
+          <h1>QnA</h1>
+        </header>
+        <table class="table">
+          <thead class="Rechead">
+            <tr class="headrow">
+              <th scope="col">글번호</th>
+              <th scope ="col" colspan="4">제목</th>
+              <th scope="col">작성자</th>
+              <th scope="col">작성일</th>
+              <th scope="col">조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(no,idx) in boards" :key ="idx">
+              <td scope="row">{{pageNum * pageSize + idx + 1}}</td>
+              <td colspan="4"><router-link :to ="{
+                  path: '/qnacontentspage/' + no.boardId
+                }" style="color: #000000">
+              {{no.title}}</router-link></td>
+              <td>{{no.name}}</td>
+              <td>{{no.boardCreatedAt}}</td>
+              <td>{{no.views}}</td>
+            </tr>
+          </tbody>
+        </table>
+        <nav aria-label="Page navigation example" style="display:flex; justify-content: center; padding:5px;">
+          <ul class="pagination">
+            <li class="page-item">
+              <a v-on:click="substractPageNum" class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">{{ pageNum + 1}}</a></li>
+            <li class="page-item">
+              <a v-on:click="addPageNum" class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>    
+        <div class ="boardbtn">
+          <button v-on:click="routeToWritePage" type="button" class="btn btn-primary" id="boardwritingbtn">글쓰기</button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -60,7 +60,7 @@ export default {
       boards: { name: '', boardId: '', title: '', boardType: '', views: '',boardCreatedAt:'' },
       length: '',
       pageNum: 0,
-      pageSize: 5,
+      pageSize: 10,
     };
   }, 
   methods: {
